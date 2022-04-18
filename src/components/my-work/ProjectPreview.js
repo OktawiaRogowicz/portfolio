1f33345b91801a2a0dc7dee8d4e710813a91225d
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-04-18 19:34:49 +0200

    Fixed order, cleaned up code

--- src/components/my-work/ProjectPreview.js
+++ src/components/my-work/ProjectPreview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Work.css";
-import styled from "styled-components";
-import { useMediaQuery } from 'react-responsive';
+import styled, {keyframes} from "styled-components";
+import {useMediaQuery} from 'react-responsive';
 import image1 from "./projectElements/ascii_game.png";
 import image2 from "./projectElements/fat16.png";
 import image3 from "./projectElements/bot_battles.png";
@@ -11,6 +11,7 @@ import image6 from "./projectElements/bot_battles.png";
 import image7 from "./projectElements/chat-app-CSS-illustration.png";
 import image8 from "./projectElements/ip-address-tracker.png";
 import image9 from "./projectElements/sunnyside-agency-landing-page.png";
+import "animate.css";
 
 const GButton = styled.button`
   background-color: transparent;
@@ -20,12 +21,12 @@ const GButton = styled.button`
   display: inline-flex;
   font-family: 'Comfortaa', cursive;
   text-transform: uppercase;
-  
-  &:hover{
+
+  &:hover {
     cursor: pointer;
     text-decoration: underline;
   }
-  
+
 `
 
 const GithubButton = (props) => {
@@ -43,15 +44,44 @@ const GithubButton = (props) => {
 const VisitProject = styled.a`
   background-color: transparent;
   border: none;
-  color: lightgray;
+  color: black;
   text-align: right;
-  display: inline-flex;
+  display: inline-block;
   font-family: 'Comfortaa', cursive;
   text-transform: uppercase;
-  
-  &:hover{
+
+  &:hover {
     cursor: pointer;
     text-decoration: underline;
+    
+    p {
+      opacity: 1;
+      transform: translateY(0);
+      animation: bounce 0.8s 0.08s;
+    }
+  }
+  
+  p {
+    opacity: 0;
+    color: white;
+    background-color: black;
+    border-radius: 5px;
+    padding: 5px;
+    width: fit-content;
+    margin: 0.5rem auto 0 auto;
+    text-transform: none;
+    text-decoration: none;
+    text-underline: none;
+    transform: translateY(-1rem);
+    transition: 0.1s all linear;
+
+    @media only screen and (max-width: 768px) {
+      opacity: 1;
+
+      background-color: transparent;
+      color: lightgray;
+      text-transform: uppercase;
+    }
   }
 
   img {
@@ -61,7 +91,7 @@ const VisitProject = styled.a`
     min-width: 150px;
     max-height: 300px;
     object-fit: scale-down;
-  
+
     @media only screen and (max-width: 768px) {
       max-width: 300px;
       flex-grow: 1;
@@ -76,7 +106,8 @@ const Container = styled.div`
   margin-bottom: 10vh;
   display: inline-flex;
   margin-left: 2rem;
-  
+  order: ${props => props.order};
+
   @media only screen and (max-width: 768px) {
     display: inline-block;
     text-align: left;
@@ -89,7 +120,7 @@ const Subcontainer = styled.div`
   width: 50%;
   text-align: center;
   flex-grow: 1;
-  
+
   p, h3, h4 {
     text-align: left;
   }
@@ -108,38 +139,28 @@ const ProjectPreview = (props) => {
     const source = props.source;
     const link = props.link;
     const leafArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
-    const isMobile = useMediaQuery({ query: `(max-width: 768px)` });
+    const isMobile = useMediaQuery({query: `(max-width: 768px)`});
 
-    if(isMobile || number % 2 === 1) {
-        return (
-            <Container>
-                <Subcontainer className="project-image-container sm">
-                    <VisitProject href={link}>
-                        <img src={leafArray[number-1]} alt="A screen of the project"/>
-                    </VisitProject>
-                    <p><span>Visit the website</span></p>
-                </Subcontainer>
-                <Subcontainer className="ProjectDescription sm">
-                    <h3>{title}</h3>
-                    <h4>{subtitle}</h4>
-                    <p>{desc}</p>
-                    <GithubButton source={source}/>
-                </Subcontainer>
-            </Container>);
-    }
     return (
         <Container>
-            <Subcontainer className="ProjectDescription sm" style={{textAlign: "right"}}>
-                <h3 style={{textAlign: "right"}}>{title}</h3>
-                <h4 style={{textAlign: "right"}}>{subtitle}</h4>
-                <p style={{textAlign: "right"}}>{desc}</p>
-                <GithubButton source={source}/>
-            </Subcontainer>
-            <Subcontainer className="project-image-container sm">
+            <Subcontainer
+                className="project-image-container sm"
+                style={isMobile || number % 2 == 1 ? {order: "1"} : {order: "2"}}
+            >
                 <VisitProject href={link}>
-                    <img className="ProjectImage" src={leafArray[number-1]} alt="A screen of the project"/>
+                    <img src={leafArray[number - 1]} alt="A screen of the project"/>
+                    <p><span>Visit the website</span></p>
                 </VisitProject>
-                <p><span>Visit the website</span></p>
+
+            </Subcontainer>
+            <Subcontainer
+                className="ProjectDescription sm"
+                style={isMobile || number % 2 == 1 ? {order: "2"} : {order: "1"}}
+            >
+                <h3 style={isMobile || number % 2 == 1 ? {} : {textAlign: "right"}}>{title}</h3>
+                <h4 style={isMobile || number % 2 == 1 ? {} : {textAlign: "right"}}>{subtitle}</h4>
+                <p style={isMobile || number % 2 == 1 ? {} : {textAlign: "right"}}>{desc}</p>
+                <GithubButton source={source}/>
             </Subcontainer>
         </Container>);
 }
